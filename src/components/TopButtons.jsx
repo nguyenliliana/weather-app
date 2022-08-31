3d69f08a0bbfbe1f5b20a3bad46dff21bbd9ff9d
Liliana <liliananguyen03@gmail.com>
2022-08-30 22:11:21 -0700

    create top buttons

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function TopButtons() {
+  const cities = [
+   {
+    id:1, 
+    title: 'London'
+   },
+   {
+    id:2, 
+    title: 'Sydney'
+   },
+   {
+    id:3, 
+    title: 'Tokyo'
+   },
+   {
+    id:4, 
+    title: 'Toronto'
+   },
+   {
+    id:5, 
+    title: 'Paris'
+   },
+  ]
+  return (
+    <div>TopButtons</div>
+  )
+}
+
+export default TopButtons
