4e43a104c90cbc675baa4fb7c66f748e658f3872
Liliana <liliananguyen03@gmail.com>
2022-09-07 20:44:37 -0700

    hard code for TimeAndLocation component

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return (
+    <div>
+     <div className="flex items-center justify-center my-6">
+      <p className="text-white text-xl font-extralight">
+        Wednesday, 7 September 2022 | Local time: 8:38pm
+      </p>
+     </div>
+     <div className="flex items-center justify-center my-3">
+      <p className="text-white text-3xl font-medium"> Anaheim, CA</p>
+     </div>
+    </div>
+  )
+}
+
+export default TimeAndLocation
