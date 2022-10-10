8cb05cac76b3aac5127f7633dadaeddd277ff7ee
nguyenliliana <liliananguyen@ucsb.edu>
2022-10-10 00:23:45 -0700

    Inputs done

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import { UilSearch,UilLocationPoint } from '@iconscout/react-unicons';
+import { getSuggestedQuery } from '@testing-library/react';
+import {useState } from 'react';
 
+function Inputs({setQuery, units, setUnits}) {
+  const [city, setCity] = useState("");
 
-function Inputs() {
+  const handleSearchClick = () => {
+    if (city !== '') setQuery({q: city})
+  }
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+        
+        setQuery ({
+          lat, 
+          lon, 
+        })
+      })
+    }
+  }
+
   return (
     <div className="flex flex-row justify-center my-6">
      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
       <input 
-       type="text" 
-       placeholder="Search for city..."
-       className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase" 
+        value={city}
+        onChange={(e) => setCity(e.currentTarget.value)}
+        type="text" 
+        placeholder="Search for city..."
+        className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase" 
       />
       <UilSearch 
         size={25} 
         className="text-white cursor-pointer transition ease-out hover:scale-125" 
+        onClick = {handleSearchClick}
       />
       <UilLocationPoint 
         size={25} 
         className="text-white cursor-pointer transition ease-out hover:scale-125"
+        onClick = {handleLocationClick}
       />
      </div>
       <div className="flex flex-row w-1/4 items-center justify-center">
